03a9ff9f977d694757efd3087a52763a97577373
sundongzhe <sundongzhe@kuaishou.com>
2025-06-09 17:59:22 +0800

    nframes 64

--- diffueraser/diffueraser.py
+++ diffueraser/diffueraser.py
@@ -255,7 +255,7 @@ class DiffuEraser:
 
     def forward(self, frames, masks, priori, output_path,
                 max_img_size = 1280, video_length=2, mask_dilation_iter=4,
-                nframes=30, seed=None, revision = None, guidance_scale=None, blended=True):
+                nframes=64, seed=None, revision = None, guidance_scale=None, blended=True):
         """
         主要的处理函数，完成视频修复的整个流程
         
