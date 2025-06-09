f9634476b466ab96fe32b1f43930a5578b303de1
sundongzhe <sundongzhe@kuaishou.com>
2025-06-09 18:38:23 +0800

    2-Step

--- run_diffueraser.py
+++ run_diffueraser.py
@@ -75,7 +75,7 @@ def main():
     ## model initialization
     device = get_device()
     # PCM params
-    ckpt = "LCM-Like LoRA"
+    ckpt = "2-Step"
     video_inpainting_sd = DiffuEraser(device, args.base_model_path, args.vae_path, args.diffueraser_path, ckpt=ckpt)
     propainter = Propainter(args.propainter_model_dir, device=device)
     
